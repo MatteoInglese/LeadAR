c05f5597061734d9bd761282fbada911d48e5a2a
Matteo Inglese <lncln1809@gmail.com>
2022-08-11 18:44:07 +0200

    Aggiunto json Interest. Aggiunta classe Interest. Aggiunta lista di punti di interesse a VPSManager

---
+++ Assets/Scripts/Interest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interest
+{
+    [SerializeField] public double Latitude { get; set; }
+    [SerializeField] public double Longitude { get; set; }
+    [SerializeField] public double Altitude { get; set; }
+    [SerializeField] public string Nome { get; set; }
+    [SerializeField] public string Text { get; set; }
+    [SerializeField] public bool HasPath { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Interest(double lat, double lon, double alt, string name, string txt, bool hasPath)
+    {
+        Latitude = lat;
+        Longitude = lon;
+        Altitude = alt;
+        Nome = name;
+        Text = txt;
+        HasPath = hasPath;
+    }
+}
