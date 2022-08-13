5d57b5c2530a5f3fc7b54099857b3acc6dc75fe8
Matteo Inglese <lncln1809@gmail.com>
2022-08-13 10:24:19 +0200

    Modificata struttura di Interest

--- Assets/Scripts/Common/Interest.cs
+++ Assets/Scripts/Common/Interest.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Interest
 {
-    [SerializeField] public double Latitude { get; set; }
+    [SerializeField] public EarthPosition position { get; set; }
+    /*[SerializeField] public double Latitude { get; set; }
     [SerializeField] public double Longitude { get; set; }
-    [SerializeField] public double Altitude { get; set; }
+    [SerializeField] public double Altitude { get; set; }*/
     [SerializeField] public string Nome { get; set; }
     [SerializeField] public string Text { get; set; }
     [SerializeField] public bool HasPath { get; set; }
@@ -24,9 +25,9 @@ public class Interest
 
     public Interest(double lat, double lon, double alt, string name, string txt, bool hasPath)
     {
-        Latitude = lat;
-        Longitude = lon;
-        Altitude = alt;
+        position.Latitude = lat;
+        position.Longitude = lon;
+        position.Altitude = alt;
         Nome = name;
         Text = txt;
         HasPath = hasPath;
